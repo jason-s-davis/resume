55abb93f842b3c5a927e041f70aba2b48f7107d1
Jason Davis <jdavis37@ggc.edu>
2014-11-05 16:37:51 -0500

    [update] moved to jade templates

--- index.js
+++ index.js
@@ -1,14 +1,54 @@
+/**
+ * Module dependencies.
+ */
 
-var express = require('express')
+var express = require('express');
+var compress = require('compression');
+var _ = require('lodash');
+var connectAssets = require('connect-assets');
+var path = require('path');
+
+
+var hour = 3600000;
+var day = hour * 24;
+var week = day * 7;
+
+
+/**
+ * Server setup.
+ */
+
 var app = express();
 
-app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
+app.set('port', (process.env.PORT || 5000));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+app.use(compress());
+app.use(connectAssets({
+  paths: [path.join(__dirname, 'public/css'), path.join(__dirname, 'public/js')],
+  helperContext: app.locals
+}));
 
-app.get('/', function(request, response) {
-      response.send('Hello World!')
-})
+// app.use(express.static(__dirname + '/public'));
+app.use(express.static(path.join(__dirname, 'public'), { maxAge: week }));
+
+
+
+
+
+app.get('/', function(req, res) {
+    // res.send('Hello World!');
+
+    res.render('home', {
+        title: 'Home'
+    });
+});
+
+app.get('/stuff', function(req, res) {
+    res.send('...');
+});
+
 
 app.listen(app.get('port'), function() {
-      console.log("Node app is running at localhost:" + app.get('port'))
-})
+  console.log("Node app is running at localhost:" + app.get('port'));
+});
